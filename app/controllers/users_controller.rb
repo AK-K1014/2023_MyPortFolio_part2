1d4b171be4f41a4c5da0dae1db2a8c9bd06d4fa0
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-07-06 11:35:04 +0900

    カラム修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:description, :image)
+    params.require(:user).permit(:introduction, :image)
   end
 end

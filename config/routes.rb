53b8bd6c20f4521e30a8c6836fff73a7333d59d7
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-07-07 18:31:55 +0900

    レイアウト修正&ログイン時にログアウトボタンヘッダーの表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  root "top#home"
-  get  "top/home"
-  post '/users', to: 'users#create'
-  get  '/users', to: 'users#show'
-  get  '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  patch '/users/:id', to: 'users#update'
-  get  '/login', to: "sessions#new"
-  post '/login', to: "sessions#create"
+  root   'top#home'
+  post   '/users',          to: 'users#create'
+  get    '/users',          to: 'users#show'
+  get    '/users/:id/edit', to: 'users#edit',       as: 'edit_user'
+  patch  '/users/:id',      to: 'users#update'
+  get    '/login',          to: "sessions#new"
+  post   '/login',          to: "sessions#create"
+  delete '/logout',         to: "sessions#destroy", as: 'logout'
+  get    '/skills/index',   to: "skills#index",     as: 'index_skill'
+  patch  '/skills/:id',     to: "skills#update"
+  # post   '/skills/new',      to: "skill#new"
+  # post   '/skills/new',      to: "skills#create"
+  # delete '/skills/:id',      to: "skill#delete"  
   resources :users
 end

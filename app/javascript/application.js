d838b273fc389b0fbff6546b420398c38c5a4c93
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-07-16 14:42:13 +0900

    スキルチャート表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require popper
 //= require bootstrap
+require("chartkick")
+require("chart.js")
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./custom/image_upload"

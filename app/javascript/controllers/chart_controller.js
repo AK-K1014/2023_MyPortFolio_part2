079b6371a212189b95dab8b7d7439161bac71e62
{KUDO} <{onigirithannsugithann@yahoo.co.jp}>
2023-07-18 06:53:44 +0900

    スキルチャート機能の修正

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -5,6 +5,7 @@ Chart.register(BarController, BarElement, CategoryScale, LinearScale);
 
 export default class extends Controller {
   static values = {
+    categoryIds: Array,
     skillNames: Array,
     skillLevels: Array
   }
@@ -15,58 +16,36 @@ export default class extends Controller {
 
   createChart() {
     const ctx = this.element.querySelector('canvas').getContext('2d');
+    const categoryIds = this.categoryIdsValue || [];
     const skillNames = this.skillNamesValue || [];
     console.log(skillNames)
     const skillLevels = this.skillLevelsValue || [];
     console.log(skillLevels)
 
+    const datasets = skillNames.map((categoryData, i) => {
+      const categoryId = categoryIds[i];
+      const skillLevelsForCategory = skillLevels.filter(data => data.category_id === categoryId);
+      return {
+        label: categoryData.category_name,
+        data: skillLevelsForCategory[0]?.skill_levels || [],
+        borderColor:[
+          'rgba(25, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)'],
+        backgroundColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)'
+        ]
+      }
+    })
+
     new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['backend', 'frontend', 'infra'],
-        datasets: [{
-          label: skillNames,
-          data: skillLevels,
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          backgroundColor: [
-            'rgba(25, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)'
-          ]
+        labels: skillNames.map(categoryData => categoryData.category_name),
+        datasets: datasets
         },
-        {
-          label: skillNames,
-          data: skillLevels,
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          backgroundColor: [
-            'rgba(25, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)'
-          ]
-        },
-        {
-          label: skillNames,
-          data: skillLevels,
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)'
-          ],
-          backgroundColor: [
-            'rgba(25, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)'
-          ]
-        }]
-      },
       options: {
         scales: {
           x: {
@@ -78,7 +57,6 @@ export default class extends Controller {
           }
         }
       }
-    },
-    );
+    },);
   }
 }
